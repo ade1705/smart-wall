d29859f36b081e69e1da6ffcb30bd33f3b238976
ade <adedoja@everlytic.com>
2020-03-12 12:25:22 +0200

    Unsplash Component now working

--- src/components/Image/ImageFetcher.ts
+++ src/components/Image/ImageFetcher.ts
@@ -1,23 +1,28 @@
 export default class ImageFetcher {
 	fetch:any;
-	private apiURL = "https://api.unsplash.com/photos/?client_id=wRCf7t-E5yWSLTXc7u52z_WbEpIek355JmKJxJHL40o";
+	private apiURL = "https://api.unsplash.com/search/photos/?client_id=wRCf7t-E5yWSLTXc7u52z_WbEpIek355JmKJxJHL40o";
 
 	constructor(fetch: any) {
 		this.fetch = fetch;
 	}
 
-	async get() {
-		let response = await this.fetch(this.apiURL);
-		return await response.json();
-	}
-
-	async getImages(searchQuery:string = '') {
+	async getImages(searchQuery:string = 'sing') {
 		try{
-			let response = await this.get();
-			return this.mapResponse(response);
+			console.log(this.prepareURL(searchQuery));
+			let response = await this.get(this.prepareURL(searchQuery));
+			return this.mapResponse(response.results);
 		}catch(err){
 			throw new Error(err)
 		}
+	}
+
+	async get(url: string) {
+		let response = await this.fetch(url);
+		return await response.json();
+	}
+
+	prepareURL(searchQuery: string): string {
+		return `${this.apiURL}&query=${searchQuery}`;
 	}
 
 	mapResponse(rawArray: any): Array<string> {
--- src/components/Image/Unsplash.tsx
+++ src/components/Image/Unsplash.tsx
@@ -1,45 +1,76 @@
 import React, {useEffect, useState} from "react";
 import Masonry from 'react-masonry-css';
-import {Box, Image, FormControl, FormLabel, Input} from "@chakra-ui/core/dist";
+import {
+	Box,
+	Image,
+	FormControl,
+	Input,
+	Menu,
+	Button,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Icon,
+	Heading
+} from "@chakra-ui/core/dist";
 import ImageFetcher from "./ImageFetcher";
 
 const composedImages = (images: Array<string>) => images.map((image:string, index:number) =>
 	<Box shadow="sm" key={index}>
-		<Image src={image} alt="Segun Adebayo" />
+		<Image src={image} />
+		<Box className="image-actions">
+			<Menu>
+				<MenuButton>
+					<Icon name="sun" color="white"/>
+				</MenuButton>
+				<MenuList>
+					<MenuItem>Set as Wallpaper</MenuItem>
+					<MenuItem>Download</MenuItem>
+				</MenuList>
+			</Menu>
+		</Box>
 	</Box>
 );
 
 export const Unsplash:React.FC = () => {
 	const [isLoading, setLoading] = useState(false);
-	const [searchQuery, setSearchQuery] = useState<string>('');
+	const [searchQuery, setSearchQuery] = useState<string>('cape town');
 	const [images, setImages] = useState<Array<string>>([]);
 	let imageFetcher = new ImageFetcher(window.fetch.bind(window));
 
 	useEffect(() => {
-		(async function() {
-			setImages(await imageFetcher.getImages(searchQuery));
-		})();
-	});
+		try {
+			(async function() {
+				console.log(await imageFetcher.getImages(searchQuery));
+				setImages(await imageFetcher.getImages(searchQuery));
+			})();
+		} catch (error) {
+			console.log(error);
+		}
+	}, [searchQuery]);
 
-	const handleSearch = (e: React.ChangeEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		console.log(e);
+	const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+		const element = event.currentTarget as HTMLFormElement;
+		setSearchQuery(element.searchQuery.value);
+		event.preventDefault();
 	};
 
 	return <>
 		<Box shadow="sm" padding={5}>
 			<form onSubmit={handleSearch}>
 				<FormControl marginBottom={5} borderRadius={0}>
-					<Input id="fname" name="searchQuery" placeholder="Search for an image"/>
+					<Input name="searchQuery" placeholder="Search for an image"/>
 				</FormControl>
 			</form>
-			<Masonry
-				breakpointCols={2}
-				className="my-masonry-grid"
-				columnClassName="my-masonry-grid_column"
-			>
-				{composedImages(images)}
-			</Masonry>
+			<Box height={310} overflow="auto">
+				<Masonry
+					breakpointCols={2}
+					className="my-masonry-grid"
+					columnClassName="my-masonry-grid_column"
+				>
+					{composedImages(images)}
+				</Masonry>
+			</Box>
 		</Box>
 		</>
 }
